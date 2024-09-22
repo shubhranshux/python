79148e2052bd0f63c1714be09618ea2f8c18ce09
Shubhranshu Behera <shubhranshu1012@gmail.com>
2024-09-22 09:21:21 +0530

    New Projects

---
+++ jarvisp1/client.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+ 
+# pip install openai 
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(
+  api_key="<3f2cdccacaa54b4587e6e8082a3dda49>",
+)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud"},
+    {"role": "user", "content": "what is coding"}
+  ]
+)
+
+print(completion.choices[0].message.content)
---
+++ jarvisp1/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "starboy":"https://youtu.be/34Na4j8AVgA?si=eRMqIuJnB2MokY0L",
+    "skyfall": "https://youtu.be/DeumyOzKqgI?si=ASyzxY_ba0nc4CZD",
+    "millionaire" : "https://youtu.be/XO8wew38VM8?si=PI41bcALYjfiKd6B",
+    "aunty": "https://youtu.be/JgDnA28O4ho?si=TcaSfXtTj35ZM8e2",
+    "dheere":"https://youtu.be/nCD2hj6zJEc?si=T5rh2qjXNhI4YDH7"
+}
